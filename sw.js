49e19904ea530c2f00eb14e4507e8c156587dc95
Sebvie974 <sebastien.vienne@gmail.com>
2026-04-26 09:13:27 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'astrodash-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Stratégie : Réseau d'abord, sinon Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
