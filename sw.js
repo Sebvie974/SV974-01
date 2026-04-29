3abca970f4c6e878a379de4635a34b5247ccdb4a
Sebvie974 <sebastien.vienne@gmail.com>
2026-04-29 17:54:19 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 const CACHE_NAME = 'astrodash-v1';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2534/2534125.png'
 ];
 
 // Installation du Service Worker
